8128d8d8c0c2a902a10c8e93a5d5d828ae498beb
Juan Sóto <juansheba97@gmail.com>
2023-06-02 00:28:12 +0100

    fixed bug in movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -181,12 +181,12 @@ const MovieDetails = () => {
           <div className="my-4">
   <p className="text-lg font-bold">Rating:</p>
   <div className="flex items-center justify-start">
-    <div style={{ width: "100px", height: "100px", marginRight: "10px" }}>
+    <div style={{ width: "120px", height: "120px", marginRight: "10px" }}>
       <CircularProgressbar 
         value={movie.vote_average * 10} 
         text={`${movie.vote_average * 10}%`} 
         styles={buildStyles({
-          textSize: '16px',
+          textSize: '24px',
           pathColor: `rgba(62, 152, 199, ${Math.min(movie.vote_average / 10, 1)})`,
           textColor: '#f88',
           trailColor: '#d6d6d6',
@@ -194,98 +194,106 @@ const MovieDetails = () => {
       />
     </div>
     <div>
-      <p className="text-lg font-semibold">{movie.vote_average}</p>
+      <p className="text-xl font-semibold">{movie.vote_average}</p>
       <p className="text-sm text-gray-500">({movie.vote_count} votes)</p>
     </div>
   </div>
 </div>
 {loading ? (
-  <Puff color="#00BFFF" height={100} width={100} />  // Este es un ejemplo de un indicador de carga. Reemplázalo por el que estés usando.
+  <div className="flex items-center justify-center mt-4">
+    <Puff color="#00BFFF" height={50} width={50} />  
+  </div>
 ) : (
-  <>
+  <div className="flex items-center justify-center mt-4">
     <button 
-      className={`font-bold py-2 px-4 rounded ${isInWishlist ? "bg-blue-700" : "bg-blue-500 hover:bg-blue-700"} text-white`}
+      className={`font-bold py-2 px-4 rounded-full shadow-md ${isInWishlist ? "bg-blue-700" : "bg-blue-500 hover:bg-blue-800"} text-white mr-2`}
       onClick={addToWishlist}
     >
       {isInWishlist ? "Remove from wishlist" : "Add to wishlist"}
     </button>
     <button 
-      className={`font-bold py-2 px-4 rounded ${isInWatchedlist ? "bg-green-700" : "bg-green-500 hover:bg-green-700"} text-white`}
+      className={`font-bold py-2 px-4 rounded-full shadow-md ${isInWatchedlist ? "bg-green-700" : "bg-green-500 hover:bg-green-800"} text-white`}
       onClick={watchedWishlist}
     >
       {isInWatchedlist ? "Remove from watchedlist" : "Add to watchedlist"}
     </button>
-  </>
+  </div>
 )}
-           <div>
-        <h3 className="text-xl font-semibold mb-2">Cast:</h3>
-        <div className="flex justify-between items-center">
-          <button 
-            onClick={prevActors} 
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            disabled={carouselIndex === 0}
-          >
-            Anterior
-          </button>
-          <div className="flex space-x-4">
-            {cast.length > 0 && cast.slice(carouselIndex, carouselIndex + actorsToShow).map(actor => (
-              <div key={actor.cast_id} className="flex flex-col items-center">
-                <img
-                  src={actor.profile_path ? `https://image.tmdb.org/t/p/w500${actor.profile_path}` : DefaultAvatar}
-                  alt={actor.name}
-                  className="w-24 h-24 rounded-3xl mr-4 shadow"
-                />
-                <div>
-                  <div className="font-bold text-lg mb-2 text-gray-900 border-b">{actor.name}</div>
-                  <p className="text-gray-700 leading-relaxed">{actor.character}</p>
-                </div>
-              </div>
-            ))}
+
+         <div>
+  <div className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 mt-5">
+    <button 
+      onClick={prevActors} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center"
+      disabled={carouselIndex === 0}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+      </svg>
+    </button>
+    <div className="flex space-x-4 overflow-auto">
+      {cast.length > 0 && cast.slice(carouselIndex, carouselIndex + actorsToShow).map(actor => (
+        <div key={actor.cast_id} className="flex flex-col items-center space-y-2 flex">
+          <img
+            src={actor.profile_path ? `https://image.tmdb.org/t/p/w500${actor.profile_path}` : DefaultAvatar}
+            alt={actor.name}
+            className="lg:w-32 xl:w-48 rounded-3xl mr-4 shadow"
+          />
+          <div>
+            <div className="font-bold lg:text-xl xl:text-2xl mb-2 text-gray-900 border-b">{actor.name}</div>
+            <p className="text-gray-700 leading-relaxed">{actor.character}</p>
           </div>
-          <button 
-            onClick={nextActors} 
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            disabled={carouselIndex === cast.length - actorsToShow}
-          >
-            Siguiente
-          </button>
         </div>
-      </div>
+      ))}
+    </div>
+    <button 
+      onClick={nextActors} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center"
+      disabled={carouselIndex === cast.length - actorsToShow}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+      </svg>
+    </button>
+  </div>
+</div>
+
         </div>
       </div>
       <div>
-        <h3 className="text-xl font-semibold mb-2">Reviews:</h3>
-        <div className="flex flex-wrap -m-2">
-          {reviews.map((review, index) => (
-            <div key={index} className="w-full sm:w-1/2 p-2">
-              <div className="bg-gray-200 p-4 rounded-lg shadow-lg flex items-start">
-                <img
-                  src={
-                    review.author_details.avatar_path && review.author_details.avatar_path.startsWith('/https')
-                      ? review.author_details.avatar_path.replace(/^\//, '')
-                      : review.author_details.avatar_path
-                        ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`
-                        : DefaultAvatar
-                  }
-                  alt={review.author}
-                  className="w-16 h-16 rounded-full mr-4 shadow"
-                />
-                <div>
-                  <div className="font-bold text-lg mb-2 text-gray-900 border-b">{review.author}</div>
-                  <div className={`overflow-hidden transition-all duration-500 ease-in-out ${expandedReviews[index] ? 'max-h-full' : 'max-h-20'}`}>
-                    <p className="text-gray-700 leading-relaxed">{review.content}</p>
-                  </div>
-                  {review.content.length > 220 && (
-                    <button onClick={() => handleExpandClick(index)} className="mt-2 text-blue-500 hover:text-blue-600">
-                      {expandedReviews[index] ? 'Show less' : 'Show more'}
-                    </button>
-                  )}
-                </div>
-              </div>
+  <h3 className="text-xl font-semibold mb-2">Reviews:</h3>
+  <div className="flex flex-wrap -m-2">
+    {reviews.map((review, index) => (
+      <div key={index} className="w-full sm:w-1/2 p-2">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-lg flex items-start">
+          <img
+            src={
+              review.author_details.avatar_path && review.author_details.avatar_path.startsWith('/https')
+                ? review.author_details.avatar_path.replace(/^\//, '')
+                : review.author_details.avatar_path
+                  ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`
+                  : DefaultAvatar
+            }
+            alt={review.author}
+            className="w-16 h-16 rounded-full mr-4 shadow"
+          />
+          <div className="w-full min-w-0"> {/* Add min-w-0 class */}
+            <div className="font-bold text-lg mb-2 text-gray-900 border-b">{review.author}</div>
+            <div className={`overflow-auto transition-all duration-500 ease-in-out ${expandedReviews[index] ? 'max-h-full' : 'max-h-20'}`}>
+              <p className="text-gray-700 leading-relaxed">{review.content}</p>
             </div>
-          ))}
+            {review.content.length > 220 && (
+              <button onClick={() => handleExpandClick(index)} className="mt-2 text-blue-500 hover:text-blue-600">
+                {expandedReviews[index] ? 'Show less' : 'Show more'}
+              </button>
+            )}
+          </div>
         </div>
       </div>
+    ))}
+  </div>
+</div>
+
     </div>
   );  
 };
