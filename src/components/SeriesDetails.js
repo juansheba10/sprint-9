b304795c4f5a7c507e7d046897ecf0c1adb832c4
Juan Sóto <juansheba97@gmail.com>
2023-06-03 10:44:33 +0100

    Fix actors carousel

--- src/components/SeriesDetails.js
+++ src/components/SeriesDetails.js
@@ -12,6 +12,8 @@ const SeriesDetails = () => {
   const [reviews, setReviews] = useState([]);
   const [expandedReviews, setExpandedReviews] = useState({})
   const [cast, setCast] = useState([]);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
 
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=7be72508776961f3948639fbd796bccd`)
@@ -49,6 +51,16 @@ const SeriesDetails = () => {
     });
   };
 
+  const actorsToShow = 3;  // Cambia este valor según cuántos actores quieres mostrar a la vez.
+
+  const nextActors = () => {
+    setCarouselIndex((prevIndex) => Math.min(prevIndex + actorsToShow, cast.length - actorsToShow));
+  };
+
+  const prevActors = () => {
+    setCarouselIndex((prevIndex) => Math.max(prevIndex - actorsToShow, 0));
+  };
+
 
   if (!series) {
     return <div className="flex justify-center items-center h-screen">
@@ -82,60 +94,77 @@ const SeriesDetails = () => {
             <p>({series.vote_count} votes)</p>
           </div>
           <div>
-            <h3 className="text-xl font-semibold mb-2">Cast:</h3>
-            <div className="flex flex-wrap -m-2">
-              {cast.slice(0, 5).map((actor, index) => (
-                <div key={index} className="w-full sm:w-1/3 p-2">
-                  <div className="bg-gray-200 p-4 rounded shadow-lg flex items-start">
-                    <img
-                      src={actor.profile_path ? `https://image.tmdb.org/t/p/w500${actor.profile_path}` : DefaultAvatar}
-                      alt={actor.name}
-                      className="w-24 h-24 rounded-3xl mr-4 shadow"
-                    />
-                    <div>
-                      <div className="font-bold text-lg mb-2 text-gray-900 border-b">{actor.name}</div>
-                      <p className="text-gray-700 leading-relaxed">{actor.character}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+  <div className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 mt-5">
+    <button 
+      onClick={prevActors} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center"
+      disabled={carouselIndex === 0}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+      </svg>
+    </button>
+    <div className="flex space-x-4 overflow-auto">
+      {cast.length > 0 && cast.slice(carouselIndex, carouselIndex + actorsToShow).map(actor => (
+        <div key={actor.cast_id} className="flex flex-col items-center space-y-2 flex">
+          <img
+            src={actor.profile_path ? `https://image.tmdb.org/t/p/w500${actor.profile_path}` : DefaultAvatar}
+            alt={actor.name}
+            className="lg:w-32 xl:w-48 rounded-3xl mr-4 shadow"
+          />
+          <div>
+            <div className="font-bold lg:text-xl xl:text-2xl mb-2 text-gray-900 border-b">{actor.name}</div>
+            <p className="text-gray-700 leading-relaxed">{actor.character}</p>
           </div>
+        </div>
+      ))}
+    </div>
+    <button 
+      onClick={nextActors} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center"
+      disabled={carouselIndex === cast.length - actorsToShow}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+      </svg>
+    </button>
+  </div>
+</div>
         </div>
       </div>
       <div>
-        <h3 className="text-xl font-semibold mb-2">Reviews:</h3>
-        <div className="flex flex-wrap -m-2">
-          {reviews.map((review, index) => (
-            <div key={index} className="w-full sm:w-1/2 p-2">
-              <div className="bg-gray-200 p-4 rounded-lg shadow-lg flex items-start">
-                <img
-                  src={
-                    review.author_details.avatar_path && review.author_details.avatar_path.startsWith('/https')
-                      ? review.author_details.avatar_path.replace(/^\//, '')
-                      : review.author_details.avatar_path
-                        ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`
-                        : DefaultAvatar
-                  }
-                  alt={review.author}
-                  className="w-16 h-16 rounded-full mr-4 shadow"
-                />
-                <div>
-                  <div className="font-bold text-lg mb-2 text-gray-900 border-b">{review.author}</div>
-                  <div className={`overflow-hidden transition-all duration-500 ease-in-out ${expandedReviews[index] ? 'max-h-full' : 'max-h-20'}`}>
-                    <p className="text-gray-700 leading-relaxed">{review.content}</p>
-                  </div>
-                  {review.content.length > 220 && (
-                    <button onClick={() => handleExpandClick(index)} className="mt-2 text-blue-500 hover:text-blue-600">
-                      {expandedReviews[index] ? 'Show less' : 'Show more'}
-                    </button>
-                  )}
-                </div>
-              </div>
+  <h3 className="text-xl font-semibold mb-2">Reviews:</h3>
+  <div className="flex flex-wrap -m-2">
+    {reviews.map((review, index) => (
+      <div key={index} className="w-full sm:w-1/2 p-2">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-lg flex items-start">
+          <img
+            src={
+              review.author_details.avatar_path && review.author_details.avatar_path.startsWith('/https')
+                ? review.author_details.avatar_path.replace(/^\//, '')
+                : review.author_details.avatar_path
+                  ? `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`
+                  : DefaultAvatar
+            }
+            alt={review.author}
+            className="w-16 h-16 rounded-full mr-4 shadow"
+          />
+          <div className="w-full min-w-0"> {/* Add min-w-0 class */}
+            <div className="font-bold text-lg mb-2 text-gray-900 border-b">{review.author}</div>
+            <div className={`overflow-auto transition-all duration-500 ease-in-out ${expandedReviews[index] ? 'max-h-full' : 'max-h-20'}`}>
+              <p className="text-gray-700 leading-relaxed">{review.content}</p>
             </div>
-          ))}
+            {review.content.length > 220 && (
+              <button onClick={() => handleExpandClick(index)} className="mt-2 text-blue-500 hover:text-blue-600">
+                {expandedReviews[index] ? 'Show less' : 'Show more'}
+              </button>
+            )}
+          </div>
         </div>
       </div>
+    ))}
+  </div>
+</div>
     </div>
   );  
 };
